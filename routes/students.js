e5026205962b19d5edeeb1fbba596bd513922ae4
James Kim <jamesjkim8806@gmail.com>
2020-11-24 11:59:07 -0800

    proj files added

---
+++ routes/students.js
@@ -0,0 +1,24 @@
+var router = require('express').Router();
+var studentsCtrl = require('../controllers/students');
+
+// GET /students
+router.get('/students', studentsCtrl.index);
+
+// POST /facts
+// We will already have access to the logged in student on
+// the server, therefore do not use: /students/:id/facts
+router.post('/facts', isLoggedIn, studentsCtrl.addFact);
+
+// DELETE /facts/:id
+router.delete('/facts/:id', studentsCtrl.delFact);
+
+// define authorization function
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }else{
+    res.redirect('/auth/google');
+  }
+}
+
+module.exports = router;
